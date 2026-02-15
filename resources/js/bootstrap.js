9e38156b2a2c948fa4372b0b126535c463d5104c
ArisAntonioCo <arisantonioco@gmail.com>
2026-02-15 22:10:17 +0800

    fix: update Bootstrap import

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import * as bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+window.bootstrap = bootstrap;
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
